451685e448cb4fc0b707ab480ab026cf11955f4f
BrianLondon <brianmlondon@gmail.com>
2022-06-21 21:48:52 -0400

    Pickle detection working

--- flake8_pickle.py
+++ flake8_pickle.py
@@ -1,7 +1,7 @@
 """Flake8 module to prevent use of Pickle"""
 import ast
 import sys
-from typing import Any, Generator, Tuple, Type
+from typing import Any, Generator, List, Tuple, Type
 
 if sys.version_info < (3, 8):
     import importlib_metadata as impmd
@@ -10,15 +10,36 @@ else:
 
 
 _PICKLE_MSG = "PKL000: Unsafe use of pickle module"
+_PANDAS_MSG = "PKL001: Unsafe use of Pandas pickle functionality"
+
+
+class PickleVisitor(ast.NodeVisitor):
+    def __init__(self) -> None:
+        self.problems: List[Tuple[int, int, str]] = []
+
+    def visit_Import(self, node: ast.Import) -> None:
+        for name in node.names:
+            if name.name == "pickle":
+                self.problems.append((node.lineno, node.col_offset, _PICKLE_MSG))
+        self.generic_visit(node)
+
+    def visit_ImportFrom(self, node: ast.ImportFrom) -> None:
+        if node.module == "pickle":
+            self.problems.append((node.lineno, node.col_offset, _PICKLE_MSG))
+        self.generic_visit(node)
 
 
 class Plugin(object):
     name = __name__
     version = impmd.version(__name__)
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, tree: ast.AST) -> None:
+        self._tree = tree
 
     def run(self) -> Generator[Tuple[int, int, str, Type[Any]], None, None]:
-        yield 1, 0, _PICKLE_MSG, type(self)
+        pickle_visitor = PickleVisitor()
+        pickle_visitor.visit(self._tree)
+        for lineno, colno, msg in pickle_visitor.problems:
+            yield lineno, colno, msg, type(self)
+
 
--- tests/test_flake8_pickle.py
+++ tests/test_flake8_pickle.py
@@ -1,5 +1,91 @@
+import ast
+from typing import Set
+
 from flake8_pickle import Plugin
 
 
+_MSG_000 = "PKL000: Unsafe use of pickle module"
+_MSG_001 = "PKL001: Unsafe use of Pandas pickle functionality"
+
+
+def _result(code: str) -> Set[str]:
+    tree = ast.parse(code)
+    plugin = Plugin(tree)
+    return {(line, col, msg) for line, col, msg, _ in plugin.run()}
+
+
 def test_version():
-    assert Plugin.__version__ == '0.1.0'
+    assert Plugin.version == '0.1.0'
+
+
+def test_empty():
+    assert _result("") == set()
+
+
+def test_import_pickle():
+    assert _result("import pickle") == {(1, 0, _MSG_000)}
+
+
+def test_import_other():
+    assert _result("import requests") == set()
+
+
+def test_import_multi():
+    assert _result("import requests, pickle") == {(1, 0, _MSG_000)}
+
+
+def test_import_from_pickle():
+    assert _result("from pickle import dump") == {(1, 0, _MSG_000)}
+
+
+def test_import_from_other():
+    assert _result("from datetime import time") == set()
+
+
+_import_in_method_code = """# comment
+def write_to_foo(s):
+    from pickle import dump
+    with open('foo', 'wb') as f:
+        dump(s, f)
+"""
+
+
+def test_import_in_method():
+    assert _result(_import_in_method_code) == {(3, 4, _MSG_000)}
+
+
+def test_import_alias_pickle():
+    assert _result("import pickle as pandas") == {(1, 0, _MSG_000)}
+
+
+def test_import_alias_other():
+    assert _result("import pandas as pickle") == set()
+
+
+_import_conditional_code = """
+if condition:
+    import json as serialization
+else:
+    import pickle as serialization
+"""
+
+def test_import_conditional():
+    assert _result(_import_conditional_code) == {(5, 4, _MSG_000)}
+
+
+_import_from_conditional_code = """
+if condition:
+    from pickle import dump
+else:
+    from json import dump
+"""
+
+def test_import_conditional():
+    assert _result(_import_from_conditional_code) == {(3, 4, _MSG_000)}
+
+
+def test_import_multiple():
+    assert _result("from pickle import dump\nfrom pickle import dumps") == {
+        (1, 0, _MSG_000),
+        (2, 0, _MSG_000)
+    }
